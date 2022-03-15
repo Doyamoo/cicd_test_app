3429d1530f61369120e1713797339dadd0136cc5
Doyamoo <ryosuke.tanimura+dev@gmail.com>
2022-03-15 21:59:12 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const MAX_LENGTH = 11
+
+class App extends Component {
+  input = ''
+  user_name = ''
+
+  constructor(props) {
+    super(props)
+    this.state  = {
+      title: 'Input form',
+      message: 'type your name.'
+    }
+    this.doChange = this.doChange.bind(this)
+    this.doSubmit = this.doSubmit.bind(this)
+
+  }
+
+  doChange(event) {    
+    this.input = event.target.value
+  }
+
+  doSubmit(event) {
+
+    if (this.input === "") {
+      this.user_name = "名無し"
+    } else if (this.input.length >= MAX_LENGTH) {
+      this.user_name = "ERROR!!"
+    } else {
+      this.user_name = this.input
+    }
+  
+    this.setState({
+      title: 'Send form',
+      message: 'こんにちは、' + this.user_name + ' さん!'
+    })
+    event.preventDefault()
+  }
+
+  render() {
+    return <div>
+      <h1 className='bg-primary text-white display-4'>React</h1>
+      <div className='container'>
+        <h4>{this.state.title}</h4>
+        <p className='card h5 p-3'>{this.state.message}</p>
+        <div className='alert alert-primary mt-3'>
+          <form onSubmit={this.doSubmit}>
+            <div className='form-group'>
+              <label>Name:</label>
+              <input type="text" className='form-control' onChange={this.doChange} />
+            </div>
+            <input type="submit" className='btn btn-primary' value="Click" />
+          </form>
+        </div>
+      </div>
     </div>
-  );
+  }
 }
 
 export default App;
